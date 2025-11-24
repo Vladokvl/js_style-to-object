1f72cefa31cef525647a6ea2bd746b88d5b5658b
Vlad Zhovtani <zovtanyy@gmail.com>
2025-11-24 22:10:39 +0200

    done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,24 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const rules = sourceString.split(';');
+
+  for (let rule of rules) {
+    rule = rule.trim();
+
+    if (rule) {
+      const [property, ...valueParts] = rule.split(':');
+
+      if (property && valueParts.length > 0) {
+        const value = valueParts.join(':').trim();
+
+        result[property.trim()] = value;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
